ab19d4f916a29830c89fa0bc1758e3111631b8d8
Daivd Iceberg <libingshan@bupt.edu.cn>
2015-11-24 00:47:33 +0800

    creat-temp.cpp
    Creat a temp.cpp file for c++ programming!

---
+++ temp.cpp
@@ -0,0 +1,15 @@
+/*This is a test file for the c++ programming!*/
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a;
+  int b;
+  int c;
+  a = 2;
+  b = 4;
+  c = a + b;
+  cout<<"the sum of a,b is c ="<<c<<endl;
+  return 0;
+  
+}
